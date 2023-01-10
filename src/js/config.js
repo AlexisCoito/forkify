168dd6c82c1c0990683e1072a81dbb58e5534c6a
AlexisCoito <acoitopaz@gmail.com>
2023-01-10 20:35:09 -0300

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'c32d75b8-ad32-4969-88ae-ab8a6ec0b4c3';
+export const MODAL_CLOSE_SEC = 2.5;
